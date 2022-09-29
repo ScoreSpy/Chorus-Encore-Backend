436a4f804f01a08878c7c9050dde5f84c7348a98
Ahriana <ahrianadev@gmail.com>
2022-09-29 22:03:02 +0100

    .chart parser

--- src/modules/helpers.ts
+++ src/modules/helpers.ts
@@ -1,5 +1,11 @@
+import { BinaryLike, createHash } from 'crypto'
+
 export class Getters {
-    static get isProduction (): boolean {
-        return process.env.NODE_ENV === 'production'
-    }
- }
+  static get isProduction (): boolean {
+    return process.env.NODE_ENV === 'production'
+  }
+}
+
+export function createMD5 (data: BinaryLike) {
+  return createHash('md5').update(data).digest('hex')
+}
