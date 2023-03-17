024ed22bf5beb390bc4671f19b8b953085a0c44f
Ahriana <ahrianadev@gmail.com>
2023-03-17 11:58:28 +0000

    add new checksums property

--- src/routes/api/v1/_user/POST_ingestSongs.ts
+++ src/routes/api/v1/_user/POST_ingestSongs.ts
@@ -173,6 +173,20 @@ const schema = {
                 }
               }
             },
+            checksums: {
+              type: 'object',
+              required: ['chart', 'archive'],
+              properties: {
+                archive: { type: 'string' },
+                chart: {
+                  type: 'object',
+                  properties: {
+                    mid: { type: 'string' },
+                    chart: { type: 'string' }
+                  }
+                }
+              }
+            },
             files: {
               type: 'object',
               required: ['video', 'image', 'stems', 'chart', 'config'],
@@ -238,22 +252,7 @@ const schema = {
   }
 }
 
-export type ChorusDiffMapString = {
-  x: string;
-  h: string;
-  m: string;
-  e: string;
-}
-
-export type ChorusDiffMapNumber = {
-  x: number;
-  h: number;
-  m: number;
-  e: number;
-}
-
 export type SongData = {
-  checksum: string,
   iniData: {
     album_track?: string
     album?: string
@@ -289,47 +288,13 @@ export type SongData = {
     video_start_time?: string
     year?: string
   },
-  chartData: {
-    hasSections: boolean
-    hasStarPower: boolean
-    hasForced: boolean
-    hasSoloSections: boolean
-    hasTap: boolean
-    hasLyrics: boolean
-    is120: boolean
-    hasBrokenNotes: boolean
-    hasOpen: {
-      guitar?: boolean
-      bass?: boolean
-      rhythm?: boolean
-      keys?: boolean
-      drums?: boolean
-      guitarghl?: boolean
-      bassghl?: boolean
-    }
-    noteCounts: {
-      guitar?: ChorusDiffMapNumber
-      bass?: ChorusDiffMapNumber
-      rhythm?: ChorusDiffMapNumber
-      keys?: ChorusDiffMapNumber
-      drums?: ChorusDiffMapNumber
-      guitarghl?: ChorusDiffMapNumber
-      bassghl?: ChorusDiffMapNumber
-    }
-    hashes: {
-      file: string,
-      guitar?: ChorusDiffMapString
-      bass?: ChorusDiffMapString
-      rhythm?: ChorusDiffMapString
-      keys?: ChorusDiffMapString
-      drums?: ChorusDiffMapString
-      guitarghl?: ChorusDiffMapString
-      bassghl?: ChorusDiffMapString
-    }
-    chartMeta: {
-      length: number,
-      effectiveLength: number
-    }
+  chartData: { },
+  checksums: {
+    chart: {
+      mid: string | null,
+      chart: string | null,
+    },
+    archive: string
   },
   files: {
     video: {
