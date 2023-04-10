9158fbf646b4de5994909027f139bc7574df40e7
Ahriana <ahrianadev@gmail.com>
2023-04-10 21:08:35 +0100

    remove debug route

--- src/routes/api/v1/_public/index.ts
+++ src/routes/api/v1/_public/index.ts
@@ -1,9 +1,5 @@
 import { FastifyInstance, FastifyServerOptions } from 'fastify'
 
-import GET_ping from './GET_ping'
-
 export default function RouteIndex (server: FastifyInstance, options: FastifyServerOptions, next: CallableFunction) {
-  server.register(GET_ping)
-
   next()
 }
